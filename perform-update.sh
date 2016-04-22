a88779f0b99954067295b9a9e7d5cf326dd08c73
pslootweg <paul.slootweg@gmail.com>
2016-04-22 18:06:28 +0100

    Add UFW config

--- perform-update.sh
+++ perform-update.sh
@@ -1,10 +1,24 @@
 #/bin/bash
 
+# run non interactive
 export DEBIAN_FRONTEND=noninteractive
 
+# Patch server
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get autoremove
 
+# Install Uncomplicated Firewall
 sudo apt-get -y install ufw
 
+# Configure UFW
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw allow ssh
+
+# Eable UFW
+sudo ufw --force enable
+
+# Reboot
+sudo reboot
